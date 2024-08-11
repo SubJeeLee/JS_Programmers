fa0cb5c109e853d15a34ee38eb9e4cbbf62bb2d9
JunSubLee <leejs010605@naver.com>
2024-08-11 14:56:53 +0000

    8.11

--- Day13/[8.11]컨트롤제트.js
+++ Day13/[8.11]컨트롤제트.js
@@ -12,3 +12,17 @@ function solution(s) {
 }
 
 console.log(solution("10 Z 20 Z"));
+
+//잘 푼 풀이
+//스택의 개념을 이용한 풀이
+// +target --> 단항덧셈연산자 = 문자열을 숫자로 변환가능
+function solution(s) {
+    const stack = []
+
+    s.split(' ').forEach((target) => {
+        if(target === 'Z') stack.pop();
+        else stack.push(+target)
+    })
+
+    return stack.length ? stack.reduce((pre, cur) => pre + cur) : 0;
+}
