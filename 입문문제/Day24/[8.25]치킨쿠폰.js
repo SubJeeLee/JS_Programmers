681b796855aaaaad2e1b6d1a6d883bdd36f84317
JunSubLee <leejs010605@naver.com>
2024-08-25 14:34:51 +0000

    8.25

---
+++ 입문문제/Day24/[8.25]치킨쿠폰.js
@@ -0,0 +1,14 @@
+function solution(chicken) {
+    function getDecimalPart(number) {
+        return number % 1;
+    }
+    let coupon = chicken;
+    while(coupon < 10){
+        let service = coupon/10;
+    }
+    let sum = 0;
+    return sum;
+}
+
+console.log(solution(100));
+console.log(solution(1081));
