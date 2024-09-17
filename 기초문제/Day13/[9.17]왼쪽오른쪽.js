d0b1b708ad2875df52b15d5f2afe5e9924fb7c82
JunSubLee <leejs010605@naver.com>
2024-09-17 14:10:14 +0000

    9.17

---
+++ 기초문제/Day13/[9.17]왼쪽오른쪽.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'l') return arr.slice(0, i);
+        if (arr[i] === 'r') return arr.slice(i + 1);
+    }
+    return [];
+}
+
+
+console.log(solution(["r", "u", "u", "u"]));
