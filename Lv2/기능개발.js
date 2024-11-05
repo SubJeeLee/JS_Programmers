94a976d2e8fb7d66eca02498f3e742de4f45c8c3
JunSubLee <leejs010605@naver.com>
2024-11-05 11:55:00 +0000

    11.05

--- Lv2/기능개발.js
+++ Lv2/기능개발.js
@@ -1,4 +1,31 @@
 function solution(progresses, speeds) {
+  let arr = progresses.map((e,i) => Math.ceil((100 - e)/speeds[i]));
+  let answer = [];
+  console.log(arr);
+  
+  let key = arr[0];
+  let cnt = 0;
+  arr.forEach(e => {
+      if(e > key){
+          answer.push(cnt);
+          key = e;
+          cnt = 1;
+      }
+      else{
+          cnt++;
+      }
+  })
+  answer.push(cnt);
+  return answer;
+}
+
+// progresses	speeds	return
+// [93, 30, 55]	[1, 30, 5]	[2, 1]
+// [95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
+
+
+//다른풀이
+function solution(progresses, speeds) {
   let answer = []
 
 for(let i=0;i<progresses.length;i++){
