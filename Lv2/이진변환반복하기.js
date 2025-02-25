09fe96a0595e6ee53b757142e2ed11030113c335
Lee Junsub <leejs010605@naver.com>
2025-02-25 19:10:58 +0900

    02.25

--- Lv2/이진변환반복하기.js
+++ Lv2/이진변환반복하기.js
@@ -10,6 +10,22 @@ function solution(s) {
     return [cnt,cntZero];
 }
 
+
+// function solution(s) {
+//     let Zerocnt = 0;
+//     let cnt = 0;
+//     let tmp = s;
+    
+//     while (true){
+//         let x = tmp.split('').filter(e => e !== '0').join('').length;
+//         Zerocnt += (tmp.length - x);
+//         tmp = x.toString(2);
+//         cnt++;
+//         if(tmp === "1") break;
+//     }
+//     return [cnt,Zerocnt];
+// }
+
 // s	result
 // "110010101001"	[3,8]
 // "01110"	[3,3]
