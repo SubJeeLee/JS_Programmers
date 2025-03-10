c2858711602a9fece418a81ae662b002db6ae96b
Lee Junsub <leejs010605@naver.com>
2025-03-10 22:10:42 +0900

    03.10

--- Lv2/전화번호목록.js
+++ Lv2/전화번호목록.js
@@ -1,14 +1,28 @@
 function solution(phoneBook) {
-    phoneBook.sort();
-    console.log(phoneBook);
-
-    for (let i = 0; i < phoneBook.length - 1; i++) {
-        if (phoneBook[i + 1].startsWith(phoneBook[i])) {
-            return false;
+    const phoneSet = new Set(phoneBook);
+    
+    for (const number of phoneBook) {
+        for (let i = 1; i < number.length; i++) {
+            if (phoneSet.has(number.slice(0, i))) {
+                return false;
+            }
         }
     }
+    
     return true;
 }
+
+// function solution(phoneBook) {
+//     phoneBook.sort();
+//     console.log(phoneBook);
+
+//     for (let i = 0; i < phoneBook.length - 1; i++) {
+//         if (phoneBook[i + 1].startsWith(phoneBook[i])) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
 // phone_book	return
 // ["119", "97674223", "1195524421"]	false
