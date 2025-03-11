125fd08d260566cb83c4c941110732d4f26dcc8a
Lee Junsub <leejs010605@naver.com>
2025-03-11 22:07:30 +0900

    03.11

--- Lv2/귤고르기.js
+++ Lv2/귤고르기.js
@@ -1,22 +1,43 @@
 function solution(k, tangerine) {
-    let obj = {};
+    let countMap = new Map();
+
+    // 귤 크기별 개수 세기
+    for (let t of tangerine) {
+        countMap.set(t, (countMap.get(t) || 0) + 1);
+    }
+
+    // 개수를 내림차순으로 정렬
+    let sortedCounts = [...countMap.values()].sort((a, b) => b - a);
+
     let cnt = 0;
-    // 각 귤의 종류별로 갯수를 세기
-    tangerine.forEach(e => {
-        obj[e] = (obj[e] || 0) + 1;
-    });
-    const sortedA = Object.values(obj).sort((a, b) => b - a);
-    //console.log(sortedA);
-    
-    for(let n of sortedA){
+    for (let n of sortedCounts) {
         k -= n;
         cnt++;
-        if(k <= 0){
-            return cnt;
-        }
+        if (k <= 0) return cnt;
     }
+    
     return cnt;
 }
+
+// function solution(k, tangerine) {
+//     let obj = {};
+//     let cnt = 0;
+//     // 각 귤의 종류별로 갯수를 세기
+//     tangerine.forEach(e => {
+//         obj[e] = (obj[e] || 0) + 1;
+//     });
+//     const sortedA = Object.values(obj).sort((a, b) => b - a);
+//     //console.log(sortedA);
+    
+//     for(let n of sortedA){
+//         k -= n;
+//         cnt++;
+//         if(k <= 0){
+//             return cnt;
+//         }
+//     }
+//     return cnt;
+// }
 
 // k	tangerine	result
 // 6	[1, 3, 2, 5, 4, 5, 2, 3]	3
