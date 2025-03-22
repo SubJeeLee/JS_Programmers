3f19893573af4de80aa42ae0e66a2e4f6ae542cc
Lee Junsub <leejs010605@naver.com>
2025-03-22 21:55:49 +0900

    03.22

--- Lv2/영어끝말잇기.js
+++ Lv2/영어끝말잇기.js
@@ -1,17 +1,33 @@
 function solution(n, words) {
-    let result =[];
-    let arr =[words[0],];
-    
-    for(let i = 1;i<words.length; i++){
-        let L = arr[arr.length-1].length
-        if(words[i][0] !== arr[arr.length-1][L-1] || arr.includes(words[i])){
-            result.push((i%n)+1,parseInt(i/n)+1);
-            break;
+    let seenWords = new Set(); 
+    let lastWord = words[0]; 
+    seenWords.add(lastWord); 
+
+    for (let i = 1; i < words.length; i++) {
+        if (lastWord[lastWord.length - 1] !== words[i][0] || seenWords.has(words[i])) { 
+            return [i % n + 1, Math.floor(i / n) + 1];
         }
-        arr.push(words[i]);
+        seenWords.add(words[i]);
+        lastWord = words[i];
+    }
+    
+    return [0, 0]; 
 }
-   return result.length === 0 ? [0,0] : result;
-}
+
+// function solution(n, words) {
+//     let result =[];
+//     let arr =[words[0],];
+    
+//     for(let i = 1;i<words.length; i++){
+//         let L = arr[arr.length-1].length
+//         if(words[i][0] !== arr[arr.length-1][L-1] || arr.includes(words[i])){
+//             result.push((i%n)+1,parseInt(i/n)+1);
+//             break;
+//         }
+//         arr.push(words[i]);
+// }
+//    return result.length === 0 ? [0,0] : result;
+// }
 
 // n	words	result
 // 3	["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]	[3,3]
